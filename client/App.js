001b7edee3b4b18f3a84158e65ca3dfcdc591886
happi89 <farhaanp99@gmail.com>
2022-08-25 14:45:31 -0400

    added graphql server

--- client/App.js
+++ client/App.js
@@ -1,10 +1,17 @@
-import Main from './src/components/Main';
 import { NativeRouter } from 'react-router-native';
+import { ApolloProvider } from '@apollo/client';
+
+import Main from './src/components/Main';
+import createApolloClient from './src/utils/apolloClient';
+
+const ApolloClient = createApolloClient();
 
 export default function App() {
 	return (
 		<NativeRouter>
-			<Main />
+			<ApolloProvider client={ApolloClient}>
+				<Main />
+			</ApolloProvider>
 		</NativeRouter>
 	);
 }
--- client/src/components/RepositoryList/Stats.jsx
+++ client/src/components/RepositoryList/Stats.jsx
@@ -1,6 +1,6 @@
-import Text from './Text';
+import Text from '../Text';
 import { StyleSheet, View } from 'react-native';
-import theme from '../theme';
+import theme from '../../theme';
 
 const styles = StyleSheet.create({
 	stats: {
